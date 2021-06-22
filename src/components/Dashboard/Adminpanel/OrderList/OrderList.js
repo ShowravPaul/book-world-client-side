6d037cc9537ca750e77f83c905955d9e17be08e2
ShowravPaul <76900484+ShowravPaul@users.noreply.github.com>
2021-06-23 02:33:34 +0600

    Update OrderList.js

--- src/components/Dashboard/Adminpanel/OrderList/OrderList.js
+++ src/components/Dashboard/Adminpanel/OrderList/OrderList.js
@@ -6,7 +6,7 @@ const OrderList = () => {
     const [orders, setOrders] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/orders')
+        fetch('https://project-3-2-book-world.herokuapp.com/orders')
             .then(response => response.json())
             .then(data => setOrders(data))
     }, [])
